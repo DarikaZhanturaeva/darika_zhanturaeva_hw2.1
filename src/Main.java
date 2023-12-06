b052131830e643a689d3902c0345142e50c2eb3c
Darika <darikazhanturaeva@gmail.com>
2023-12-06 17:50:38 +0600

    hw

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args){
+    int a = (int) (Math.random()*(100));
+
+    System.out.println(a);
+
+            System.out.println(goForAWalk(35, 17));
+            System.out.println(goForAWalk(18, 28));
+            System.out.println(goForAWalk(50, 10));
+            System.out.println(goForAWalk(5, -5));
+            System.out.println(goForAWalk(35, 17));
+        }
+        public static String goForAWalk(int age, int temperature) {
+            if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
+                    (age < 20 && temperature >= 0 && temperature <= 28) ||
+                    (age > 45 && temperature >= -10 && temperature <= 25)) {
+                return "можно идти гулять";
+            } else {
+                return "Оставайтесь дома!";
+            }
+        }
+
+    }
